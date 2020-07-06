7ba9fc983e7947b6f4561d55fc7e2f20a44e2d98
yashmunjal <yashmu45@gmail.com>
2020-07-06 22:41:47 +0530

    counter

--- js/scroll.js
+++ js/scroll.js
@@ -2,8 +2,31 @@ var a = 0;
 $(window).scroll(function() {
 
   var oTop = $('#showcase').offset().top - window.innerHeight;
-  
-  if ($(window).scrollTop() > oTop) {
-    console.log('hello');
-    };
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-value').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 1600,
+          easing: 'easeOutQuad',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
 });
